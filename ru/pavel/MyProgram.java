505d84cfc462301485c1cfe19846ab0561e66450
PavelBorovinskiy <53998632+PavelBorovinskiy@users.noreply.github.com>
2020-03-21 01:08:15 +0300

    Обновил файл

---
+++ ru/pavel/MyProgram.java
@@ -0,0 +1,7 @@
+package ru.pavel;
+
+public class MyProgram {
+public static void main(String[] args){
+System.out.println("Hello, Lera");
+}
+}
--- sandbox/src/main/java/ru/pavel/MyProgram.java
+++ sandbox/src/main/java/ru/pavel/MyProgram.java
@@ -1,7 +1,7 @@
 package ru.pavel;
 
 public class MyProgram {
-public static void main(String[] args){
-System.out.println("Hello, Lera");
-} 
+    public static void main(String[] args) {
+        System.out.println("Hello, Lera");
+    }
 }
