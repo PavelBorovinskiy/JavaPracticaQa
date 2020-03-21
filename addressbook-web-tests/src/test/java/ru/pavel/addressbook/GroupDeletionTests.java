58e849e52a10ba4e7b0ed4284f472e7ab6134dba
PavelBorovinskiy <53998632+PavelBorovinskiy@users.noreply.github.com>
2020-03-21 12:06:29 +0300

    Создание теста удаление групп
    Создали новый тест и унаследовали его от базавого класса TestBase

---
+++ addressbook-web-tests/src/test/java/ru/pavel/addressbook/GroupDeletionTests.java
@@ -0,0 +1,17 @@
+package ru.pavel.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+
+    gotoGroupPage();
+    selectdGroup();
+    deleteSelectedGroup();
+    returnToGroupPage();
+  }
+
+}
