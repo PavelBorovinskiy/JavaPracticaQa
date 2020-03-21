6491e0acb0752e04a4cd01883fae08d53f6025a4
PavelBorovinskiy <53998632+PavelBorovinskiy@users.noreply.github.com>
2020-03-21 05:10:48 +0300

    Создание методов

--- sandbox/src/main/java/ru/pavel/MyProgram.java
+++ sandbox/src/main/java/ru/pavel/MyProgram.java
@@ -1,8 +1,23 @@
 package ru.pavel;
 
 public class MyProgram {
+
   public static void main(String[] args) {
-    System.out.println("Hello, Lera");
+    hello("Pavel");
+    System.out.println();
+
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangel r = new Rectangel(8,1);
+
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
   }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody);
+  }
+
 }
- 
---
+++ sandbox/src/main/java/ru/pavel/Rectangel.java
@@ -0,0 +1,14 @@
+package ru.pavel;
+
+public class Rectangel {
+  public double a;
+  public double b;
+
+  public Rectangel(double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+  public  double area() {
+    return this.a * this.b;
+  }
+}
