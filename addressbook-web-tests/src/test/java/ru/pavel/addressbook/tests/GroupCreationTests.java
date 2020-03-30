2908482c284483559a47380eb3da0d7d224a87bb
PavelBorovinskiy <53998632+PavelBorovinskiy@users.noreply.github.com>
2020-03-30 18:13:57 +0300

    Поиск максимального индификатора через лямбда выражение

--- addressbook-web-tests/src/test/java/ru/pavel/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pavel/addressbook/tests/GroupCreationTests.java
@@ -1,10 +1,10 @@
 package ru.pavel.addressbook.tests;
 
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.pavel.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -14,9 +14,29 @@ public class GroupCreationTests extends TestBase {
 
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    GroupData group = new GroupData("test3", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+//    int max =0;               //нахождение максимального индификатора при создание группы
+//    for (GroupData g: after){
+//      if(g.getId()> max){
+//        max = g.getId();
+//      }
+//    }
+
+
+/*   Comparator<? super GroupData> byId = (Comparator<GroupData>)
+    (o1, o2) -> Integer.compare(o1.getId(),o2.getId());
+    int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId();
+
+    * нахождение максимального индификатора при создание группы, через лямбда выражение.
+
+ */
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
 
 
